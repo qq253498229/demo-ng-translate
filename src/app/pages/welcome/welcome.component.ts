3e69c2790f3bd687d4b41db335091c93e4a60c69
大胃王 <253498229@qq.com>
2025-06-05 17:57:42 +0800

    以编程方式读取文本

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { NzDividerComponent } from 'ng-zorro-antd/divider';
 import { Store } from '@ngxs/store';
 import { SwitchLanguage } from '../../store/system/system.action';
 import { SystemSelector } from '../../store/system/system.selector';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-welcome',
@@ -13,8 +14,10 @@ import { SystemSelector } from '../../store/system/system.selector';
   styleUrl: './welcome.component.scss',
   imports: [CommonModule, SharedModule, NzDividerComponent],
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
   store = inject(Store);
+  translate = inject(TranslateService);
+  text = '';
 
   test() {
     let language = this.store.selectSnapshot(SystemSelector.language());
@@ -26,5 +29,18 @@ export class WelcomeComponent {
 
   switchLanguage() {
     this.store.dispatch(new SwitchLanguage());
+  }
+
+  ngOnInit() {
+    this.loadText();
+    this.translate.onLangChange.subscribe(lang => {
+      this.loadText();
+    });
+  }
+
+  private loadText() {
+    this.translate.get('app.readByProgram').subscribe(r => {
+      this.text = r;
+    });
   }
 }
